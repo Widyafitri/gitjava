4ca9159c222bab87ba2b4b130154dd35b1e3b19a
widiya fitri <23widyafitri@gmail.com>
2022-10-08 02:22:31 +0700

    Update fitur

--- src/com/juaracoding/gitjava/Main.java
+++ src/com/juaracoding/gitjava/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         //github
         System.out.println("Hello world!");
+        System.out.println("Tambahan coding");
     }
 }
